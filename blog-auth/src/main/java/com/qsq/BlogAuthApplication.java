8c82875afced36d273fbebf923b7429ca19b8991
qsqcode <qsq1113948520@163.com>
2020-05-01 22:34:34 +0800

    qsq-blog

---
+++ blog-auth/src/main/java/com/qsq/BlogAuthApplication.java
@@ -0,0 +1,20 @@
+package com.qsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Copyright (C), 2020
+ * @FileName: BlogAuthApplication
+ * @Author: quesq
+ * @Date: 2020/5/1 21:55
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class BlogAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAuthApplication.class, args);
+    }
+
+}
